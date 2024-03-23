ed69d6d3a19b6bfd9489f477078ffea9a3f28de7
Ankan Biswas <151124403+ankandevops@users.noreply.github.com>
2024-03-23 16:23:11 +0530

    changing the vm name

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region     = "us-west-2"
 }
 
-resource "aws_instance" "myec2" {
+resource "aws_instance" "myec2vm" {
   ami           = "ami-005e54dee72cc1d00"
   instance_type = "t2.nano"
 
